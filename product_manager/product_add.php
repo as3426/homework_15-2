c580a28686caaa41eb1da259c327df3df068287f
A Sharma <as3426@njit.edu>
2017-07-29 20:07:20 -0400

    product_add.php modified

--- product_manager/product_add.php
+++ product_manager/product_add.php
@@ -14,17 +14,23 @@
         </select>
         <br>
 
-        <label>Code:</label>
-        <input type="input" name="code">
-        <br>
+	<label>Code:</label>
+        <input type="input" name="code" value="<?php echo htmlspecialchars($code);?>">
+	<?php echo $fields->getField('code')->getHTML(); ?>
+	<br>
+
 
         <label>Name:</label>
-        <input type="input" name="name">
-        <br>
+	 <input type="input" name="name" value="<?php echo htmlspecialchars($name);?>">
+	 <?php echo $fields->getField('name')->getHTML(); ?>
+	 <br>
+
 
         <label>List Price:</label>
-        <input type="input" name="price">
-        <br>
+	   <input type="input" name="price" value="<?php echo htmlspecialchars($price);?>">
+	   <?php echo $fields->getField('price')->getHTML(); ?>
+	   <br>
+
 
         <label>&nbsp;</label>
         <input type="submit" value="Add Product">
