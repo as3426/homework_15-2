162c09faf9016417cc0e7c1183946ed0a7de3c8c
A Sharma <as3426@njit.edu>
2017-07-29 20:04:35 -0400

    main.css added to index file

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php include 'view/header.php'; ?>
+ <link rel="stylesheet" type="text/css"
+           href="/~as3426/homework_15-2/main.css">
+
 <main>
     <h1>Menu</h1>
     <p><a href="product_manager">Product Manager</a></p>
